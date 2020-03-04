15d964a125a09e6a99d5288d7b9e586de3209c1f
saba383810 <saba383810@gmail.com>
2020-03-04 21:10:21 +0900

    first commit

---
+++ ActionGame1/Assets/scripts/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+
+    public GameObject GoalText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoalText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.tag == "Player") {
+            GoalText.SetActive(true);
+		}
+	}
+}
